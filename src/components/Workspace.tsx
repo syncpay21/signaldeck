86a60f21f429aa95128be4740783fcda8462f328
Ezana <syncpay21@users.noreply.github.com>
2026-05-18 18:11:07 +1000

    Auto-save

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -2735,6 +2735,7 @@ export default function Workspace({
         auditData={auditData}
         claimsData={claimsData}
         vcData={vcData}
+        signalsData={signalsData}
       />
     </div>
   )
